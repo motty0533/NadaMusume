8470fb534f4d9c9f114f35d0323dbd6b4200ac32
motty0533 <soutaro.ym@gmail.com>
2021-08-19 00:16:41 +0900

    残りポイントの表示を実装

---
+++ main.js
@@ -0,0 +1,22 @@
+$(function(){
+  
+  var single_point = 0;
+  var triple_point = $('.tripleform').val();
+  var remaining_point = 100;
+  $('.singleform').on('input',function(){
+    single_point = $('.singleform').val();
+    remaining_point = 100 - single_point - triple_point;
+    if(remaining_point<0){
+      remaining_point = 0;
+    }
+    $('.button1 span').text("残り"+remaining_point+"pt");
+  })
+  $('.tripleform').on('input',function(){
+    triple_point = $('.tripleform').val();
+    remaining_point = 100 - single_point - triple_point;
+    if(remaining_point<0){
+      remaining_point = 0;
+    }
+    $('.button1 span').text("残り"+remaining_point+"pt");
+  })
+});
