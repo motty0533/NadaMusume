902b99e36090515d386a41002b821c5c02bcc0c0
motty0533 <soutaro.ym@gmail.com>
2021-08-24 15:03:32 +0900

    トップページ途中まで

--- main.js
+++ main.js
@@ -83,9 +83,20 @@ $(function(){
         $('.js-modal').fadeOut();
         $('body,html').stop().animate({scrollTop:winScrollTop}, 100);
         return false;
-    }); 
+    });
+    
+    // トップページであと何日かを取得
+    var today = new Date();
+    var festival = new Date("2021/9/26")
+    var countdown = Math.ceil((festival.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+    if(countdown>=0){
+      $('.remaindays span').text(+countdown+"日");
+    } else{
+      $('.remaindays').text("2021年度体育祭は終了いたしました。")
+    }    
+    //トップページのマーカーのアニメーション
+    $('.remaindays').markerAnimation()
   });
-
 
 // ドラッグアンドドロップ
 jQuery(function ($) {
